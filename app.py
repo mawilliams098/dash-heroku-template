719f7efaa04155f7528c898e659516d4305b68f8
Annie Williams <37524861+mawilliams098@users.noreply.github.com>
2020-11-30 20:43:25 -0500

    added from dash import Dash

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import dash
+from dash import Dash
 from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
